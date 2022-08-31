159aaa00dc83a1b51a233d314a1c3e5b5b9c3526
kulichkoff <kulichkov@background.digital>
2022-08-31 13:19:42 +0300

    MAIN: simple currency calculations

--- main.cpp
+++ main.cpp
@@ -1,26 +1,53 @@
 #include <iostream>
+#include <stdlib.h>
+
 #include "src/http_client.h"
 #include "src/currency_parser.h"
 
 #include <curlpp/Options.hpp> // for exceptions
 
-int main() {
+int main(int argc, char **argv) {
   try {
     HttpClient http;
 
-    std::string raw_json = http.Get("https://www.cbr-xml-daily.ru/daily_json.js");
+    std::string raw_json = http.Get(
+      "https://www.cbr-xml-daily.ru/daily_json.js");
     CurrencyParser parser(raw_json.c_str());
 
-    Currency *usd = parser.GetUsd();
-    std::cout << usd->value << std::endl;
 
+    Currency *usd = parser.GetUsd();
     Currency *eur = parser.GetEur();
-    std::cout << eur->value << std::endl;
+
+    if (argc == 1) {
+      std::cout << "USD: " << usd->value << std::endl;
+      std::cout << "EUR: " << eur->value << std::endl;
+    } else if (argc == 3) {
+      std::string currency_flag = argv[1];
+      double value = atof(argv[2]);
+      std::string currency;
+      double result = 0;
+
+      if (currency_flag == "--usd") {
+        currency = "USD";
+        result = usd->value * value;
+      } else if (currency_flag == "--eur") {
+        currency = "EUR";
+        result = eur->value * value;
+      }
+
+      std::cout << "[" << currency << "] " << value << " = [RUB] " << result
+                << std::endl;
+    } else {
+      std::cerr << "Usage: currencier OR currencier --[eur|usd] [value]\n";
+      return -1;
+    }
 
   } catch (curlpp::RuntimeError &e) {
     std::cerr << e.what() << std::endl;
+    return -1;
   } catch (curlpp::LogicError &e) {
     std::cerr << e.what() << std::endl;
+    return -1;
   }
 
   return 0;
